ab0d41065780a48f01d63209520a0b02e517305f
bravos2k5 <nationalbao2005@gmail.com>
2025-01-19 15:02:40 +0700

    initialize

---
+++ src/main/java/com/bravos2k5/encryption/EncrytionApplication.java
@@ -0,0 +1,13 @@
+package com.bravos2k5.encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncrytionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncrytionApplication.class,args);
+    }
+
+}
